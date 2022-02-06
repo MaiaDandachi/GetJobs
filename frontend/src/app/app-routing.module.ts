9da4f0a994003b615b2000e8dcdb8bffe3da33f2
Maia-Dandachi <Lynn.dandachi@bilgiedu.net>
2022-02-06 19:57:51 +0300

    Add routing for job creation & home

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FollowUpPageComponent } from './components/follow-up-page/follow-up-page.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { JobCreateComponent } from './components/jobs/job-create/job-create.component';
 import { ProfileComponent } from './components/profile-page/profile.component';
 
 const routes: Routes = [
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     component: FollowUpPageComponent,
     path: 'follow-up',
+  },
+  {
+    component: JobCreateComponent,
+    path: 'create-job',
   },
   {
     component: HomePageComponent,
