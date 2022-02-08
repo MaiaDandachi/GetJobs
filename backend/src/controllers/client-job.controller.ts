67c9eb21e7b19de1e533fb801ce7245216b9f779
Maia-Dandachi <Lynn.dandachi@bilgiedu.net>
2022-02-08 15:26:56 +0300

    Add has many relation between Client, Freelancer & Job also add filter for get jobs of a freelancer.

---
+++ backend/src/controllers/client-job.controller.ts
@@ -0,0 +1,110 @@
+import {
+  Count,
+  CountSchema,
+  Filter,
+  repository,
+  Where,
+} from '@loopback/repository';
+import {
+  del,
+  get,
+  getModelSchemaRef,
+  getWhereSchemaFor,
+  param,
+  patch,
+  post,
+  requestBody,
+} from '@loopback/rest';
+import {
+  Client,
+  Job,
+} from '../models';
+import {ClientRepository} from '../repositories';
+
+export class ClientJobController {
+  constructor(
+    @repository(ClientRepository) protected clientRepository: ClientRepository,
+  ) { }
+
+  @get('/clients/{id}/jobs', {
+    responses: {
+      '200': {
+        description: 'Array of Client has many Job',
+        content: {
+          'application/json': {
+            schema: {type: 'array', items: getModelSchemaRef(Job)},
+          },
+        },
+      },
+    },
+  })
+  async find(
+    @param.path.string('id') id: string,
+    @param.query.object('filter') filter?: Filter<Job>,
+  ): Promise<Job[]> {
+    return this.clientRepository.jobs(id).find(filter);
+  }
+
+  @post('/clients/{id}/jobs', {
+    responses: {
+      '200': {
+        description: 'Client model instance',
+        content: {'application/json': {schema: getModelSchemaRef(Job)}},
+      },
+    },
+  })
+  async create(
+    @param.path.string('id') id: typeof Client.prototype.id,
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Job, {
+            title: 'NewJobInClient',
+            exclude: ['id'],
+            optional: ['clientId']
+          }),
+        },
+      },
+    }) job: Omit<Job, 'id'>,
+  ): Promise<Job> {
+    return this.clientRepository.jobs(id).create(job);
+  }
+
+  @patch('/clients/{id}/jobs', {
+    responses: {
+      '200': {
+        description: 'Client.Job PATCH success count',
+        content: {'application/json': {schema: CountSchema}},
+      },
+    },
+  })
+  async patch(
+    @param.path.string('id') id: string,
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Job, {partial: true}),
+        },
+      },
+    })
+    job: Partial<Job>,
+    @param.query.object('where', getWhereSchemaFor(Job)) where?: Where<Job>,
+  ): Promise<Count> {
+    return this.clientRepository.jobs(id).patch(job, where);
+  }
+
+  @del('/clients/{id}/jobs', {
+    responses: {
+      '200': {
+        description: 'Client.Job DELETE success count',
+        content: {'application/json': {schema: CountSchema}},
+      },
+    },
+  })
+  async delete(
+    @param.path.string('id') id: string,
+    @param.query.object('where', getWhereSchemaFor(Job)) where?: Where<Job>,
+  ): Promise<Count> {
+    return this.clientRepository.jobs(id).delete(where);
+  }
+}
---
+++ backend/src/controllers/freelancer-job.controller.ts
@@ -0,0 +1,119 @@
+import {
+  Count,
+  CountSchema,
+  Filter,
+  repository,
+  Where,
+} from '@loopback/repository';
+import {
+  del,
+  get,
+  getModelSchemaRef,
+  getWhereSchemaFor,
+  param,
+  patch,
+  post,
+  requestBody,
+} from '@loopback/rest';
+import {Freelancer, Job} from '../models';
+import {FreelancerRepository} from '../repositories';
+
+export class FreelancerJobController {
+  constructor(
+    @repository(FreelancerRepository)
+    protected freelancerRepository: FreelancerRepository,
+  ) {}
+
+  @get('/freelancers/{id}/jobs', {
+    responses: {
+      '200': {
+        description: 'Array of Freelancer has many Job',
+        content: {
+          'application/json': {
+            schema: {type: 'array', items: getModelSchemaRef(Job)},
+          },
+        },
+      },
+    },
+  })
+  async find(
+    @param.path.string('id') id: string,
+    @param.query.object('filter') filter?: Filter<Job>,
+  ): Promise<Job[]> {
+    return this.freelancerRepository.jobs(id).find({
+      include: [
+        {
+          relation: 'client',
+          scope: {
+            // further filter the client object
+            fields: {password: false}, // dont show password
+          },
+        },
+      ],
+    });
+  }
+
+  @post('/freelancers/{id}/jobs', {
+    responses: {
+      '200': {
+        description: 'Freelancer model instance',
+        content: {'application/json': {schema: getModelSchemaRef(Job)}},
+      },
+    },
+  })
+  async create(
+    @param.path.string('id') id: typeof Freelancer.prototype.id,
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Job, {
+            title: 'NewJobInFreelancer',
+            exclude: ['id'],
+            optional: ['freelancerId'],
+          }),
+        },
+      },
+    })
+    job: Omit<Job, 'id'>,
+  ): Promise<Job> {
+    return this.freelancerRepository.jobs(id).create(job);
+  }
+
+  @patch('/freelancers/{id}/jobs', {
+    responses: {
+      '200': {
+        description: 'Freelancer.Job PATCH success count',
+        content: {'application/json': {schema: CountSchema}},
+      },
+    },
+  })
+  async patch(
+    @param.path.string('id') id: string,
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Job, {partial: true}),
+        },
+      },
+    })
+    job: Partial<Job>,
+    @param.query.object('where', getWhereSchemaFor(Job)) where?: Where<Job>,
+  ): Promise<Count> {
+    return this.freelancerRepository.jobs(id).patch(job, where);
+  }
+
+  @del('/freelancers/{id}/jobs', {
+    responses: {
+      '200': {
+        description: 'Freelancer.Job DELETE success count',
+        content: {'application/json': {schema: CountSchema}},
+      },
+    },
+  })
+  async delete(
+    @param.path.string('id') id: string,
+    @param.query.object('where', getWhereSchemaFor(Job)) where?: Where<Job>,
+  ): Promise<Count> {
+    return this.freelancerRepository.jobs(id).delete(where);
+  }
+}
