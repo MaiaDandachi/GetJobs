003afeaf3d0b0d6169b784abaf69f8e697f0ea17
Maia-Dandachi <Lynn.dandachi@bilgiedu.net>
2022-02-09 14:13:49 +0300

    Add authentication service & Implement login functionality.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -36,6 +37,7 @@ import { JobCreateComponent } from './components/jobs/job-create/job-create.comp
     JobCreateComponent,
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { first } from 'rxjs/operators';
+import { AuthenticationService } from '@app/_services/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -8,7 +11,9 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   hide = true;
+  loading = false;
   submitted = false;
+  error = '';
 
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.email, Validators.required]),
@@ -16,9 +21,19 @@ export class LoginComponent implements OnInit {
       Validators.minLength(6),
       Validators.required,
     ]),
+    userRoleControl: new FormControl(false),
   });
 
-  constructor() {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {
+    // redirect to home if already logged in
+    if (this.authenticationService.userValue) {
+      this.router.navigate(['/']);
+    }
+  }
 
   ngOnInit(): void {}
 
@@ -29,9 +44,34 @@ export class LoginComponent implements OnInit {
 
   onSubmit() {
     this.submitted = true;
+
+    // return if teh form is invalid
     if (this.loginForm.invalid) {
       return;
     }
-    console.log(this.loginForm);
+
+    this.loading = true;
+    const userType = this.loginForm.value['userRoleControl']
+      ? 'clients'
+      : 'freelancers';
+
+    this.authenticationService
+      .login(
+        this.loginForm.value['email'],
+        this.loginForm.value['password'],
+        userType
+      )
+      .pipe(first())
+      .subscribe({
+        next: () => {
+          // get return url from query parameters or default to home page
+          const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+          this.router.navigateByUrl(returnUrl);
+        },
+        error: (error) => {
+          this.error = error;
+          this.loading = false;
+        },
+      });
   }
 }
